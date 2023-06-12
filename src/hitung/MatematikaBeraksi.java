b17d2d62563eb43ae254bc07ba8402a467f69266
raflifirmansucahyo <raflifirman279@gmail.com>
2023-06-12 20:27:34 +0800

    Membuat calss MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hitung;
 
-/**
- *
- * @author ASUS
- */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika rafli = new Matematika(5,0);
+        
+        System.out.println("Hasil penjumlahan: "+rafli.setPenjumlahan());
+        System.out.println("Hasil pengurangan: "+rafli.setPengurangan());
+        System.out.println("Hasil perkalian: "+rafli.setPerkalian());
+        System.out.println("Hasil pembagian: "+rafli.setPembagian());
+    }
 }
